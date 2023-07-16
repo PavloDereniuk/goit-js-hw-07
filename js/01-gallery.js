69169c4d578481e001f6c29b222db0f139fac2cf
PavloDereniuk <125708520+PavloDereniuk@users.noreply.github.com>
2023-07-16 17:30:26 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryEl = galleryItems.map(
+    ({ preview, original, description }) => 
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`);
+
+gallery.insertAdjacentHTML('beforeend', galleryEl.join(''));
+
+gallery.addEventListener('click', showOriginalImg);
+
+
+function showOriginalImg(event) {
+    event.preventDefault();
+    if(event.target.tagName !== "IMG") return ;
+
+    const { target } = event;
+
+    const examp = basicLightbox.create(
+       `<img src="${target.dataset.source}" width="800">`
+    )
+    examp.show();
+
+    gallery.addEventListener("keydown", (event) => {
+         if(event.code === 'Escape') 
+         examp.close();
+      });
+};
